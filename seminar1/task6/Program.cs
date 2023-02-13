e4154c7b0296ae3914dfae99381949f3450a2f65
Evelina <evelina.shaymardanova@list.ru>
2023-02-13 11:41:15 +0500

    added task6

---
+++ seminar1/task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает чилсло и выдает, является ли число 
+// четным(делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0) Console.WriteLine("да");
+
+else Console.WriteLine("нет");
