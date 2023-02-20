6380775ae5b2bc58cc3ebd670e2591a8527310fe
Evelina <evelina.shaymardanova@list.ru>
2023-02-20 11:33:03 +0500

    added task15

---
+++ seminar2/task15/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот деньвыходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число");
+int numDay = Convert.ToInt32(Console.ReadLine());
+if (numDay > 7)
+{
+    Console.WriteLine("Нет");
+}    
+else if (numDay < 6)
+{
+    Console.WriteLine("Нет");
+}
+else if (numDay == 6)
+{
+    Console.WriteLine("Да");
+}
+else if (numDay == 7)
+{
+    Console.WriteLine("Да");
+}
+
