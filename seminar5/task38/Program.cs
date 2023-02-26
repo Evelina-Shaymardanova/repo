e369d6ff3e5f6547adf2ac1e747a4b1ddaf06a6d
Evelina <evelina.shaymardanova@list.ru>
2023-02-26 13:57:06 +0500

    added task38

---
+++ seminar5/task38/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+//минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+var array = new decimal[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Введите {0} число : ", i+1);
+                array[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+ 
+            var max = array[0];
+            var min = array[0];
+            for (int i = 1; i < 5; i++)
+            {
+                if (array[i] < min) min = array[i];
+                if (array[i] > max) max = array[i];
+            }
+            
+            Console.WriteLine("Разность между максимальным и минимальным = {0}", max - min);
