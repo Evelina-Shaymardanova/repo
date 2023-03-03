4e7c70945de8a3257d8278818df3839ffec6d210
Evelina <evelina.shaymardanova@list.ru>
2023-03-03 10:37:55 +0500

    added task41

---
+++ seminar6/task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры неопределенное количество чисел (while (true)). Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Ввод чисел останавливается при помощи ввода слова "stop".
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+string userString = "";
+int count = 0;
+
+do
+{
+    Console.WriteLine("Введите любую строку и нажмите клавишу Enter: ");
+    userString = Console.ReadLine();
+    int value = Convert.ToInt32(userString);
+
+    if (value > 0)
+    {
+        count = count +1;
+    }
+    Console.WriteLine(count);
+} while (userString != "stop");
+
+
+
+
+ 
