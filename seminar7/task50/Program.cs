b4d7b1dbe14f8517b86c32476e6e20f6174a239d
Evelina <evelina.shaymardanova@list.ru>
2023-03-04 13:26:53 +0500

    added task50

--- seminar7/task50/Program.cs
+++ seminar7/task50/Program.cs
@@ -11,17 +11,26 @@ Console.Write("Введите строку: ");
 int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
 Console.Write("Введите столбец: ");
 int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-int n = 5; 
-int m = 7; 
+int n = 3; 
+int m = 4; 
 Random random = new Random();
-int[,] arr = new int[n, m];
+int[,] array = new int[n, m];
 Console.WriteLine("Исходный массив: ");
-for (int i = 0; i < arr.GetLength(0); i++)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int j = 0; j < array.GetLength(1); j++)
 {
-    arr[i, j] = random.Next(10, 99);
-Console.Write("{0} ", arr[i, j]);
+    array[i, j] = random.Next(0, 101);
+Console.Write("{0} ", array[i, j]);
 }
 Console.WriteLine();
 }
+    if (pos1 < 0 | pos1 > array.GetLength(0) - 1 | pos2 < 0 | pos2 > array.GetLength(1) - 1)
+{
+Console.WriteLine("Элемент не существует  ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", array[pos1, pos2]);
+}
+Console.ReadLine();
