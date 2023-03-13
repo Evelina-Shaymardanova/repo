a5d15ccb66f677404430746de215b9970ade5adc
Evelina <evelina.shaymardanova@list.ru>
2023-03-13 19:31:58 +0500

    fixed errors

--- seminar9/task68/Program.cs
+++ seminar9/task68/Program.cs
@@ -4,18 +4,19 @@
 // m = 2, n = 3 -> A(m,n) = 9
 
 Console.WriteLine("Введите начальное число M:");
-int numberM = int.Parse(Console.ReadLine());
+uint numberM = uint.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите начальное число N:");
-int numberN = int.Parse(Console.ReadLine());
+uint numberN = uint.Parse(Console.ReadLine());
 
 ///Метод вычисления функции Аккермана:
-int AckermannFunction (int numberM, int numberN)
+uint AckermannFunction (uint numberM, uint numberN)
 {
     if (numberM == 0) return numberN + 1;
-    if (numberM > 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
     if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
 return AckermannFunction(numberM, numberN);
 }
 
 Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
+
