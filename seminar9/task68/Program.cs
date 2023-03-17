2836941348a8fa6e24348502bca72cdd4f45259c
Evelina <evelina.shaymardanova@list.ru>
2023-03-17 22:15:58 +0500

    added task1

--- seminar9/task68/Program.cs
+++ seminar9/task68/Program.cs
@@ -4,10 +4,10 @@
 // m = 2, n = 3 -> A(m,n) = 9
 
 Console.WriteLine("Введите начальное число M:");
-uint numberM = uint.Parse(Console.ReadLine());
+uint numberM = uint.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите начальное число N:");
-uint numberN = uint.Parse(Console.ReadLine());
+uint numberN = uint.Parse(Console.ReadLine()!);
 
 ///Метод вычисления функции Аккермана:
 uint AckermannFunction (uint numberM, uint numberN)
