e0ed820cfa9822e1d7b6c87b9254a26eb3b1b7f3
Evelina <evelina.shaymardanova@list.ru>
2023-03-20 20:34:04 +0500

    added task7

---
+++ seminar1/task7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numb = 0;
+if (99 < number || number < 999)
+{
+    numb = number % 10;
+}
+
+Console.WriteLine(numb);
