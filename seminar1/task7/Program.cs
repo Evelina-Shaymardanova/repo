eb7f8dc793a96e8db0b02ffa08dc8cc7e3c99d1d
Evelina <evelina.shaymardanova@list.ru>
2023-03-20 20:46:00 +0500

    added task7

--- seminar1/task7/Program.cs
+++ seminar1/task7/Program.cs
@@ -4,13 +4,15 @@
 // 782 -> 2
 // 918 -> 8
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+int length = number.ToString().Length;
 
-int numb = 0;
-if (99 < number || number < 999)
+if (length==3)
 {
-    numb = number % 10;
+    Console.WriteLine(number % 10);
 }
-
-Console.WriteLine(numb);
+else 
+{
+    Console.WriteLine("Число не является трехзначным!");
+}
