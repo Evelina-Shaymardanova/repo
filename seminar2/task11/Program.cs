7cea7ffa9d55682b2ef3f5719c558ee8719beb24
Evelina <evelina.shaymardanova@list.ru>
2023-03-27 15:11:10 +0500

    added task11

---
+++ seminar2/task11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num = Random.Shared.Next(100,1000);
+Console.WriteLine(num);
+
+int num1 = (num/100)*10;
+int num2 = (num%10)+num1;
+Console.WriteLine(num2);
