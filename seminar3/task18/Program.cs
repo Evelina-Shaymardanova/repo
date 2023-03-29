9f9c85293056b947b38ce3b7dd7f0899dae14468
Evelina <evelina.shaymardanova@list.ru>
2023-03-29 14:11:43 +0500

    added task18

---
+++ seminar3/task18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер координатной четверти: ");
+int coordinata = Convert.ToInt32(Console.ReadLine());
+
+if (coordinata == 1)
+{
+    Console.WriteLine("Диапозон точек первой четверти: x > 0 и y > 0");
+}
+else if (coordinata == 2)
+{
+    Console.WriteLine("Диапозон точек второй четверти: x < 0 и y > 0");
+}
+else if (coordinata == 3)
+{
+    Console.WriteLine("Диапозон точек третьей четверти: x < 0 и y < 0");
+}
+else if (coordinata == 4)
+{
+    Console.WriteLine("Диапозон точек четвертой четверти: x > 0 и y < 0");
+}
+else
+{
+    Console.WriteLine("Нет такой координатной четверти!");
+}
